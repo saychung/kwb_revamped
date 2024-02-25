701bb090a0025464363911d05c56c9a96c87d6dc
saychung <tashiwoiserbhutiasmit@yahoo.com>
2024-02-25 13:26:22 +0530

    updated backend

--- src/app/components/landingpage.tsx
+++ src/app/components/landingpage.tsx
@@ -25,7 +25,7 @@ const TextLogo = () => (
     <div className=" w-1/2 pl-0  tabl:pl-10 tabl:top-[50%] tabl:left-[20%] text-gray-600 ">
         <div className={`w-full flex justify-center flex-col items-center `}>
             <div className={`text-[3rem] tabl:text-[4rem] grid text-left tracking-widest  `}>Khandu <br/> Wangchuk <br/> Bhutia 
-        <div className=" whitespace-nowrap"><Typer /></div>
+        {/* <div className=" whitespace-nowrap"><Typer /></div> */}
        </div> 
         
         </div>  
@@ -39,9 +39,9 @@ const LandingPage = () => (
     
     <div className="w-[100%] h-[100svh] overflow-hidden flex flex-col sm:flex-row gap-10 items-center justify-center">
     <TextLogo />
-    <span id="image" className="w-full h-1/2 relative sm:w-1/2 sm:h-dvh">
+    {/* <span id="image" className="w-full h-1/2 relative sm:w-1/2 sm:h-dvh">
         <Image src="/images/blurlandingimage.png" alt="Art Image" width={500} height={500} className="w-full h-full object-contain" />
-    </span>
+    </span> */}
     
     </div>
     <p className="absolute w-full text-black/50 bottom-5 text-center text-md z-2 hidden sm:inline">Scroll to View</p>
