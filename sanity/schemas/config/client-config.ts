1f01f5593f24b3a626c53fe6956af4d47fc36efa
saychung <tashiwoiserbhutiasmit@yahoo.com>
2025-03-25 15:34:15 +0530

    somechanges

--- sanity/schemas/config/client-config.ts
+++ sanity/schemas/config/client-config.ts
@@ -1,7 +1,6 @@
 import { ClientPerspective } from "next-sanity";
 
 const config  = {
-    
         projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
         dataset: "production",
         apiVersion: "2024-01-23",
